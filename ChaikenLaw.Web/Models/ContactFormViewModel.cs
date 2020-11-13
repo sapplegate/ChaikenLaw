32753f1d5ac5ef4fb4ba7d2d1b1f438c21309019
Steven Applegate <sapplegate86@gmail.com>
2020-11-12 22:25:50 -0700

    first checkin

---
+++ ChaikenLaw.Web/Models/ContactFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChaikenLaw.Web.Models
+{
+    public class ContactFormViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        public string Message { get; set; }
+    }
+}
